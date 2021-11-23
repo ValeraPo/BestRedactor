9b2f2e0cace94f69b9a6a1101a7f4ecb60e7e2a0
Stepan <53170355+stepa28@users.noreply.github.com>
2021-11-23 21:12:28 +0300

    Added the ability to write to a file (such as a log file)

---
+++ BestRedactor/Data/Loger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace BestRedactor.Data
+{
+    public static class Loger
+    {
+        private static string _path = "..\\..\\..\\Log";
+        public static string Path
+        {
+            get => _path;
+            set => _path = value;
+        }
+        
+        public static void Log(string logMessage)
+        {
+            if (!Directory.Exists(_path)) 
+                Directory.CreateDirectory(_path);
+
+            using (StreamWriter w = File.AppendText($"{_path}\\log {DateTime.Now.ToShortDateString()}.txt"))
+            {
+                w.Write("\r\nLog Entry : ");
+                w.WriteLine($"{DateTime.Now.ToLongTimeString()} {DateTime.Now.ToLongDateString()}");
+                w.WriteLine($"  :{logMessage}");
+                w.WriteLine("-------------------------------");
+            }
+        }
+    }
+}
