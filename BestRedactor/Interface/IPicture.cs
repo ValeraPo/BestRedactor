d2f2970813adfd74d01e1b89d306b7e1512b4fe9
Stepa <cntgf.2010@yandex.ru>
2021-11-26 13:18:46 +0300

    Add Interface

---
+++ BestRedactor/Interface/IPicture.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+
+namespace BestRedactor.Interface
+{
+    public interface IPicture
+    {
+        public Bitmap Bitmap {get; set;}
+        public string FileName {get; set;}
+        public System.Drawing.Imaging.ImageFormat ImageFormat {get; set;}
+    }
+}
---
+++ BestRedactor/Interface/ISettings.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace BestRedactor.Interface
+{
+    public interface ISettings
+    {
+        public Color LastUseColor {get; set;}
+        public uint LastUseSize {get; set;}
+        public uint OpenedTabs {get; set;}
+        public bool FailClose {get; set;}
+    }
+}
