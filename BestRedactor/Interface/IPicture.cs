8f3b641cf516ded9e8660dedfa81ec0d1eefdc30
Stepa <cntgf.2010@yandex.ru>
2021-11-26 13:40:04 +0300

    Add Directory in IPicture
    Signed-off-by: Stepa <cntgf.2010@yandex.ru>

--- BestRedactor/Interface/IPicture.cs
+++ BestRedactor/Interface/IPicture.cs
@@ -5,6 +5,7 @@ namespace BestRedactor.Interface
     public interface IPicture
     {
         public Bitmap Bitmap {get; set;}
+        public string Directory {get; set;}
         public string FileName {get; set;}
         public System.Drawing.Imaging.ImageFormat ImageFormat {get; set;}
     }
