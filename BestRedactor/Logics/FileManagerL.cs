43d385c2a333c48e16cd56f7d7d6049c8e95defc
ValeraPo <portnovavalerija@mail.ru>
2021-12-01 14:56:00 +0300

    Add File Manager to logics

---
+++ BestRedactor/Logics/FileManagerL.cs
@@ -0,0 +1,18 @@
+﻿using BestRedactor.Data;
+using BestRedactor.Interface;
+using BestRedactor.Data.AutoSave;
+
+namespace BestRedactor.Logics
+{
+    public class FileManagerL
+    {
+        public static void Save(IPicture picture) => FileManager.Save(picture);
+
+        public static IPicture Load(string path) => FileManager.Load(path);
+        
+        // На всякий случай второй способ
+        //public static IPicture Load(IPicture picture) => FileManager.Load(picture.FileName);
+
+        public static IPicture LoadFromClipboard() => FileManager.LoadFromClipboard();
+    }
+}
