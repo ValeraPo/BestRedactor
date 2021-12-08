7ffa19c5abcc6f371235175c282c13a769318340
ValeraPo <portnovavalerija@mail.ru>
2021-12-08 15:26:24 +0300

    move fill

--- BestRedactor/Forms/MainForm.cs
+++ BestRedactor/Forms/MainForm.cs
@@ -454,38 +454,6 @@ namespace BestRedactor.Forms
 
             lblPictureSize.Text = $@"{picture.Bitmap.Width} x {picture.Bitmap.Height}";
         }
-
-
-        // метод для поиска старого цвета до заливки формы новым цветом
-        private void Validate(Bitmap bm, Stack<Point> sp, int x, int y, Color oldColor, Color newColor)
-        {
-            var cx = bm.GetPixel(x, y);
-            if (cx != oldColor)
-                return;
-            sp.Push(new Point(x, y));
-            bm.SetPixel(x, y, newColor);
-        }
-        private void Fill(Bitmap bm, int x, int y, Color newCol)
-        {
-            var oldCol = bm.GetPixel(x, y);
-            var pixel  = new Stack<Point>();
-            pixel.Push(new Point(x, y));
-            bm.SetPixel(x, y, newCol);
-            if (oldCol.ToArgb() == newCol.ToArgb())
-                return;
-            while (pixel.Count > 0)
-            {
-                var pt = pixel.Pop();
-                if (pt.X <= 0 || pt.Y <= 0 || pt.X >= bm.Width - 1 || pt.Y >= bm.Height - 1)
-                    continue;
-                Validate(bm, pixel, pt.X - 1, pt.Y, oldCol, newCol);
-                Validate(bm, pixel, pt.X, pt.Y - 1, oldCol, newCol);
-                Validate(bm, pixel, pt.X + 1, pt.Y, oldCol, newCol);
-                Validate(bm, pixel, pt.X, pt.Y + 1, oldCol, newCol);
-            }
-        }
-
-
         private void drDBtnTSMenuItIncreaseContrast_Click(object sender, EventArgs e) =>
             new FiltersForm(_picture, this, Filters.Contrast).ShowDialog();
         private void drDBtnTSMenuItBlur_Click(object sender, EventArgs e)
@@ -599,7 +567,7 @@ namespace BestRedactor.Forms
         private void pictureBox_MouseClick(object sender, MouseEventArgs e)
         {
             if (_currentTool == Tools.Fill)
-                Fill(_picture.Bitmap, e.X, e.Y, Settings.LastUseColor);
+                Filling.Fill(_picture.Bitmap, e.X, e.Y, Settings.LastUseColor);
             if (_currentTool != Tools.Pipette)
                 return;
             Settings.LastUseColor = _picture.Bitmap.GetPixel(e.X, e.Y);
